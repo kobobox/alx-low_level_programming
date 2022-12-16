5b0a899c691d459b19c2b49286a400e374d8d08a
kobobox <ezediunor.damian@gmail.com>
2022-12-16 02:27:36 -0800

    dynamic libraries 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Mahmoud2227/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://raw.github.com/kobobox/alx-low_level_programming/main/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD="$PWD/../nrandom.so"
